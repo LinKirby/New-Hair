3ce48e7d7b86decdcbc91c9006379ea89ac9e1ca
LinKirby <71834622+LinKirby@users.noreply.github.com>
2020-10-20 16:54:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -326,17 +326,17 @@ jQuery(document).ready(function($) {
 
 });
 //Scrolltop function
-var mybutton = document.getElementById("myBtn");
+// var mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		mybutton.style.display = "block";}
-	else {
-		mybutton.style.display = "none";}
- }
-function topFunction() {
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
-  } 
+// function scrollFunction() {
+// 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+// 		mybutton.style.display = "block";}
+// 	else {
+// 		mybutton.style.display = "none";}
+//  }
+// function topFunction() {
+// 	document.body.scrollTop = 0;
+// 	document.documentElement.scrollTop = 0;
+//   } 
